f95e2d1df22daf9ac16cd652f5b51d53b83256c8
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-25 10:54:38 +0200

    fixed ProjectConfig bug

--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/config/ProjectConfig.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/config/ProjectConfig.java
@@ -2,7 +2,9 @@ package com.rizwanmushtaq.ElectronicStore.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ProjectConfig {
   @Bean
   public ModelMapper mapper() {
