3712b23a2431fe59818ca7b7a8fdf49d8a076cec
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-29 14:20:25 +0200

    added @Service annotation

--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/services/implementations/CategoryServiceImpl.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/services/implementations/CategoryServiceImpl.java
@@ -13,7 +13,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
   @Autowired
   private CategoryRepository categoryRepository;
