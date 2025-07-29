57ae34e2aed5f1760abd8aebef8c09d8a2d60909
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-29 10:51:38 +0200

    added serverUserImage api

--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/controllers/UserController.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/controllers/UserController.java
@@ -4,18 +4,24 @@ import com.rizwanmushtaq.ElectronicStore.dtos.ApiResponseMessage;
 import com.rizwanmushtaq.ElectronicStore.dtos.ImageResponse;
 import com.rizwanmushtaq.ElectronicStore.dtos.PageableResponse;
 import com.rizwanmushtaq.ElectronicStore.dtos.UserDto;
+import com.rizwanmushtaq.ElectronicStore.exceptions.ResourceNotFoundException;
 import com.rizwanmushtaq.ElectronicStore.services.FileService;
 import com.rizwanmushtaq.ElectronicStore.services.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -104,5 +110,22 @@ public class UserController {
         .build();
     return new ResponseEntity<>(imageResponse, HttpStatus.CREATED);
   }
+
   // serve user image
+  @GetMapping("/image/{userId}")
+  public void serveUserImage(
+      @PathVariable String userId,
+      HttpServletResponse response
+  ) throws IOException {
+    UserDto user = userService.getUserById(userId);
+    String imageName = user.getImageName();
+    logger.info("Serving image: {}", imageName);
+    if (imageName != null) {
+      InputStream resource = fileService.getResource(imageUploadPath, imageName);
+      response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+      StreamUtils.copy(resource, response.getOutputStream());
+    } else {
+      throw new ResourceNotFoundException("Image not found for user: " + userId);
+    }
+  }
 }
