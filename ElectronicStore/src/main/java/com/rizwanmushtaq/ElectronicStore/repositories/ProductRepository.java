bfa28a0c9a7fe7bd36167111ca8398d88192cce4
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-29 17:08:24 +0200

    updated ProductRepository

--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/repositories/ProductRepository.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/repositories/ProductRepository.java
@@ -3,5 +3,10 @@ package com.rizwanmushtaq.ElectronicStore.repositories;
 import com.rizwanmushtaq.ElectronicStore.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+  List<Product> findByTitleContaining(String title);
+
+  List<Product> findByLiveTrue(Boolean live);
 }
