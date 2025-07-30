6dea40cd45c03978dc703d45dd3558dc49cc1e4d
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-30 20:47:37 +0200

    added Cart Controller

---
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/controllers/CartController.java
@@ -0,0 +1,66 @@
+package com.rizwanmushtaq.ElectronicStore.controllers;
+
+import com.rizwanmushtaq.ElectronicStore.dtos.AddItemToCartRequest;
+import com.rizwanmushtaq.ElectronicStore.dtos.ApiResponseMessage;
+import com.rizwanmushtaq.ElectronicStore.dtos.CartDto;
+import com.rizwanmushtaq.ElectronicStore.services.CartService;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+  @Autowired
+  private CartService cartService;
+
+  @PostMapping("/{userId}")
+  public ResponseEntity<CartDto> addItemToCart(
+      @PathVariable String userId,
+      @RequestBody AddItemToCartRequest addItemToCartRequest
+  ) throws BadRequestException {
+    CartDto cartDto =
+        cartService.addItemToCart(userId, addItemToCartRequest);
+    return new ResponseEntity<>(cartDto, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{userId}/items/{itemId}")
+  public ResponseEntity<ApiResponseMessage> removeItemFromCart(
+      @PathVariable String itemId,
+      @PathVariable String userId
+  ) {
+    cartService.removeItemFromCart(userId, Integer.parseInt(itemId));
+    ApiResponseMessage responseMessage = ApiResponseMessage
+        .builder()
+        .message("Item removed from cart successfully")
+        .success(true)
+        .status(HttpStatus.OK)
+        .build();
+    return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{userId}")
+  public ResponseEntity<ApiResponseMessage> clearCart(
+      @PathVariable String userId
+  ) {
+    cartService.clearCart(userId);
+    ApiResponseMessage responseMessage = ApiResponseMessage
+        .builder()
+        .message("Cart cleared successfully")
+        .success(true)
+        .status(HttpStatus.OK)
+        .build();
+    return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<CartDto> getCart(
+      @PathVariable String userId
+  ) {
+    CartDto cartDto =
+        cartService.getCartByUserId(userId);
+    return new ResponseEntity<>(cartDto, HttpStatus.OK);
+  }
+}
