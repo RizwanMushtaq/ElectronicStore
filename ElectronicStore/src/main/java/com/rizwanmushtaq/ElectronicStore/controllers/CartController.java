6049a67ecee08b240794b05c3bba449d29bea6fd
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-31 09:38:50 +0200

    updated api endpoint

--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/controllers/CartController.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/controllers/CartController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/api/carts")
 public class CartController {
   @Autowired
   private CartService cartService;
