41c2b9475e2362bcd1446ba0ee7dc290059dce66
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-31 22:03:02 +0200

    added Orders controller logic

---
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/controllers/OrderController.java
@@ -0,0 +1,68 @@
+package com.rizwanmushtaq.ElectronicStore.controllers;
+
+import com.rizwanmushtaq.ElectronicStore.dtos.ApiResponseMessage;
+import com.rizwanmushtaq.ElectronicStore.dtos.CreateOrderRequest;
+import com.rizwanmushtaq.ElectronicStore.dtos.OrderDto;
+import com.rizwanmushtaq.ElectronicStore.dtos.PageableResponse;
+import com.rizwanmushtaq.ElectronicStore.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+  @Autowired
+  private OrderService orderService;
+
+  @PostMapping
+  public ResponseEntity<OrderDto> createOrder(
+      @RequestBody CreateOrderRequest createOrderRequest
+  ) {
+    OrderDto orderDto = OrderDto.builder()
+        .billingName(createOrderRequest.getBillingName())
+        .billingAddress(createOrderRequest.getBillingAddress())
+        .billingPhone(createOrderRequest.getBillingPhone())
+        .build();
+    String userId = createOrderRequest.getUserId();
+    String cartId = createOrderRequest.getCartId();
+    OrderDto createdOrderDto = orderService.createOrder(orderDto, userId,
+        cartId);
+    return new ResponseEntity<>(createdOrderDto, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{orderId}")
+  public ResponseEntity<ApiResponseMessage> removeOrder(
+      @PathVariable String orderId
+  ) {
+    orderService.removeOrder(orderId);
+    ApiResponseMessage responseMessage = ApiResponseMessage
+        .builder()
+        .message("Order removed successfully")
+        .success(true)
+        .status(HttpStatus.OK)
+        .build();
+    return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<List<OrderDto>> getOrdersOfUser(
+      @PathVariable String userId
+  ) {
+    return new ResponseEntity<>(orderService.getOrdersOfUser(userId), HttpStatus.OK);
+  }
+
+  @GetMapping
+  public ResponseEntity<PageableResponse<OrderDto>> getOrders(
+      @RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+      @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+      @RequestParam(value = "sortBy", defaultValue = "name", required = false) String sortBy,
+      @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+  ) {
+    PageableResponse<OrderDto> pageableResponse = orderService.getOrders(pageNumber, pageSize, sortBy, sortDir);
+    return new ResponseEntity<>(pageableResponse, HttpStatus.OK);
+  }
+}
--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/dtos/CreateOrderRequest.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/dtos/CreateOrderRequest.java
@@ -1,5 +1,6 @@
 package com.rizwanmushtaq.ElectronicStore.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -9,9 +10,14 @@ import lombok.*;
 @Builder
 @ToString
 public class CreateOrderRequest {
+  @NotBlank(message = "User ID cannot be blank")
   private String userId;
+  @NotBlank(message = "Cart ID cannot be blank")
   private String cartId;
+  @NotBlank(message = "Billing name cannot be blank")
   private String billingName;
+  @NotBlank(message = "Billing address cannot be blank")
   private String billingAddress;
+  @NotBlank(message = "Billing phone cannot be blank")
   private String billingPhone;
 }
