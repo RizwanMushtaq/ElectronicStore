7ea0d0c1fcd43f14fa3c68d1800edc2845eb02e8
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-10 19:15:44 +0200

    test jwt generation in test case

--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/security/JwtHelper.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/security/JwtHelper.java
@@ -4,15 +4,18 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-public class jwtHelper {
+@Component
+public class JwtHelper {
   public static final long TOKEN_VALIDITY = 5 * 60 * 1000;
-  public static final String SECRET_KEY = "secret";
+  public static final String SECRET_KEY =
+      "secrethgjjjjjjjjjjjjjjjjjjjjjjzzzzzzzzzzzzzzzzzzzztttttttttttttttttttttttttttttttttffffffffffffffffffffffffffffffggggggggg";
 
   public String getUsernameFromToken(String token) {
     return getClaimFromToken(token, Claims::getSubject);
--- ElectronicStore/src/test/java/com/rizwanmushtaq/ElectronicStore/ElectronicStoreApplicationTests.java
+++ ElectronicStore/src/test/java/com/rizwanmushtaq/ElectronicStore/ElectronicStoreApplicationTests.java
@@ -1,13 +1,27 @@
 package com.rizwanmushtaq.ElectronicStore;
 
+import com.rizwanmushtaq.ElectronicStore.entities.User;
+import com.rizwanmushtaq.ElectronicStore.repositories.UserRepository;
+import com.rizwanmushtaq.ElectronicStore.security.JwtHelper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ElectronicStoreApplicationTests {
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private JwtHelper jwtHelper;
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
+  @Test
+  void testJwtToken() {
+    User username = userRepository.findByUsername("admin").get();
+    String token = jwtHelper.generateToken(username);
+    System.out.println("tesing Token: " + token);
+  }
 }
