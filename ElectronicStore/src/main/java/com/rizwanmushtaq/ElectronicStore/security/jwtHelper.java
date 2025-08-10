e39c5b6c23e501f5c8652563b768df3e24dc0914
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-10 19:00:58 +0200

    added JwtHelper Class

---
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/security/jwtHelper.java
@@ -0,0 +1,53 @@
+package com.rizwanmushtaq.ElectronicStore.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class jwtHelper {
+  public static final long TOKEN_VALIDITY = 5 * 60 * 1000;
+  public static final String SECRET_KEY = "secret";
+
+  public String getUsernameFromToken(String token) {
+    return getClaimFromToken(token, Claims::getSubject);
+  }
+
+  public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = getAllClaimsFromToken(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims getAllClaimsFromToken(String token) {
+    return Jwts.parser().setSigningKey(SECRET_KEY).build().parseClaimsJws(token).getPayload();
+  }
+
+  public Boolean isTokenExpired(String token) {
+    final Date expiration = getExpirationDateFromToken(token);
+    return expiration.before(new Date());
+  }
+
+  public Date getExpirationDateFromToken(String token) {
+    return getClaimFromToken(token, Claims::getExpiration);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    return doGenerateToken(claims, userDetails.getUsername());
+  }
+
+  private String doGenerateToken(Map<String, Object> claims, String subject) {
+    return Jwts
+        .builder()
+        .setClaims(claims)
+        .setSubject(subject)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY))
+        .signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
+  }
+}
