652de2f2b640d871eff12f1e778d64272583c936
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-12 15:48:57 +0200

    added logic to fetch data

--- AngularApp/src/app/app.ts
+++ AngularApp/src/app/app.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -5,8 +6,22 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('AngularApp');
+
+  constructor(private http: HttpClient) {}
+
+  public loadProducts() {
+    console.log('loading products');
+    this.http.get<any>('localhost:8080/api/products').subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        console.log('Error loading products', error);
+      },
+    });
+  }
 }
