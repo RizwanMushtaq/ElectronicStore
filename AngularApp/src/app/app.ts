16e5864a80dd151f8a93b87bb90fda1b82239cc9
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-12 19:06:36 +0200

    added mui buttons

--- AngularApp/src/app/app.ts
+++ AngularApp/src/app/app.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
