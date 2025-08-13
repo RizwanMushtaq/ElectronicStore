f2bb0d33b66c40ba58a75b3bc0993c68e7ff3084
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-13 17:41:41 +0200

    updated app version

--- ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/config/OpenApiConfig.java
+++ ElectronicStore/src/main/java/com/rizwanmushtaq/ElectronicStore/config/OpenApiConfig.java
@@ -13,7 +13,7 @@ public class OpenApiConfig {
     return new OpenAPI()
         .info(new Info()
             .title("Electronic Store API")
-            .version("1.0")
+            .version("1.0.0")
             .description("API documentation for the Electronic Store project")
             .summary("This project is done to experiment with Spring Boot.")
             .license(new License().name("Apache 2.0").url("http://springdoc.org")));
