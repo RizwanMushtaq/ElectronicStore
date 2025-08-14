63b02d7527fb6eebbc438616cbe21fa45bce4ffc
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-14 22:20:48 +0200

    added tailwind css

--- frontend-app/src/pages/Home.tsx
+++ frontend-app/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return <div>Electronic Store Home Page</div>;
+  return (
+    <h1 className="text-2xl font-mono underline">Electronic Store Home Page</h1>
+  );
 };
 
 export default Home;
